499b11717d988bdabb0855309e51bc9741fbd8cb
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-27 21:20:33 +0700

    TES

--- plugins/commands.py
+++ plugins/commands.py
@@ -62,7 +62,7 @@ async def start(client, message):
 
     if AUTH_GROUPS and not await is_grup(client, message):
         try:
-            invite_link = await client.create_chat_invite_link(int(AUTH_GROUPS))
+            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except ChatAdminRequired:
             logger.error("Make sure Bot is admin in Forcesub channel")
             return
