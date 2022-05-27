42f2450afccdc6f6b799788b3871032249fcf0f4
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-27 11:40:19 +0700

    r

--- plugins/inline.py
+++ plugins/inline.py
@@ -34,6 +34,7 @@ async def answer(bot, query):
         await query.answer(results=[],
                            cache_time=0,
                            switch_pm_text='Mohon Subscribe Channel kami!',
+                           protect_content=True,
                            switch_pm_parameter="subscribe")
         return
 
