8ddd8babc7f725718f0908b6d6a188156dc8ce8a
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-28 09:22:02 +0700

    tes

--- plugins/josprojects/Photofilter.py
+++ plugins/josprojects/Photofilter.py
@@ -7,7 +7,7 @@ async def photo(client: Client, message: Message):
     try:
         await client.send_message(
             chat_id=message.chat.id,
-            text="Select your required mode from below!ㅤㅤ",
+            text="Untuk Penggunaan Fitur Hapus Background & Effect\nSilahkan untuk kirim foto dengan caption /effect.\nNanti bot akan memprosesnya.",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
