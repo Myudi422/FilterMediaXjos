7464b1c7360ba227e1c47f84d6e369c3ff44308e
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-28 09:42:17 +0700

    t

--- info.py
+++ info.py
@@ -16,6 +16,9 @@ API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
+#Downloader
+DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
+
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
