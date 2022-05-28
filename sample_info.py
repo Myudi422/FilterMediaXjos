aa9c071aecad5f3bcd4839729029d53d5f76e645
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-28 07:51:09 +0700

    tes

--- sample_info.py
+++ sample_info.py
@@ -29,7 +29,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUPS')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+AUTH_GROUPS = int(auth_grp) if auth_grp and id_pattern.search(auth_grp) else None
 
 
 # MongoDB information
