2ee1e9fef75245ea18ff044df10516721d56734a
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-28 07:37:18 +0700

    TES

--- plugins/inline.py
+++ plugins/inline.py
@@ -4,14 +4,14 @@ from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
 from database.ia_filterdb import get_search_results
 from utils import get_size, is_subscribed, temp
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from info import CACHE_TIME, AUTH_USERS, AUTH_GROUPS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
-cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
+cache_time = 0 if AUTH_GROUPS or AUTH_CHANNEL else CACHE_TIME
 
 async def inline_users(query: InlineQuery):
-    if AUTH_USERS:
-        if query.from_user and query.from_user.id in AUTH_USERS:
+    if AUTH_GROUPS:
+        if query.from_user and query.from_user.id in AUTH_GROUPS:
             return True
         else:
             return False
