6d4e5b2dfca1e749409615cc414eac30a73baf0b
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-30 16:19:18 +0700

    tes

--- plugins/josprojects/stickers.py
+++ plugins/josprojects/stickers.py
@@ -16,6 +16,9 @@ from telegram import (
 from telegram.ext import CallbackContext, run_async
 from telegram.utils.helpers import mention_html
 
+from josprojects import dispatcher
+from josprojects.modules.disable import DisableAbleCommandHandler
+
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
 
@@ -132,7 +135,7 @@ def kang(update: Update, context: CallbackContext):
         elif msg.reply_to_message.sticker and msg.reply_to_message.sticker.emoji:
             sticker_emoji = msg.reply_to_message.sticker.emoji
         else:
-            sticker_emoji = "🤔"
+            sticker_emoji = "😈"
 
         if not is_animated:
             try:
@@ -278,7 +281,7 @@ def kang(update: Update, context: CallbackContext):
                 png_sticker = urlemoji[1]
                 sticker_emoji = urlemoji[2]
             except IndexError:
-                sticker_emoji = "🤔"
+                sticker_emoji = "😈"
             urllib.urlretrieve(png_sticker, kangsticker)
             im = Image.open(kangsticker)
             maxsize = (512, 512)
@@ -445,9 +448,15 @@ def makepack_internal(
             parse_mode=ParseMode.MARKDOWN,
         )
     else:
-        msg.reply_text("Failed to create sticker pack. Possibly due to blek mejik.")
+        msg.reply_text("Failed to create sticker pack. Possibly due to I don't know 🤣.")
 
 
+__help__ = """
+╔ `/stickerid`*:* reply to a sticker to me to tell you its file ID.
+╠ `/getsticker`*:* reply to a sticker to me to upload its raw PNG file.
+╠ `/kang`*:* reply to a sticker to add it to your pack.
+╚  `/stickers`*:* Find stickers for given term on combot sticker catalogue
+"""
 
 __mod_name__ = "Stickers"
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
@@ -455,3 +464,7 @@ GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
 KANG_HANDLER = DisableAbleCommandHandler("kang", kang, admin_ok=True)
 STICKERS_HANDLER = DisableAbleCommandHandler("stickers", cb_sticker)
 
+dispatcher.add_handler(STICKERS_HANDLER)
+dispatcher.add_handler(STICKERID_HANDLER)
+dispatcher.add_handler(GETSTICKER_HANDLER)
+dispatcher.add_handler(KANG_HANDLER)
