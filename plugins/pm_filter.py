12c2bb1d03c4ebd02ad797172c96573fc7825d37
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-06-01 08:53:35 +0700

    TES

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -14,7 +14,7 @@ from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GRO
     SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.handlers import CallbackQueryHandler
-from pyrogram import Client, filters
+from pyrogram import Client, filters, Bot
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
 from database.users_chats_db import db
@@ -179,7 +179,7 @@ async def next_page(bot, query):
 async def private_give_filter(client, message):
         await auto_filter(client, message)
 
-@bot.on_message(filters.private & filters.text & filters.incoming)
+@Bot.on_message(filters.private & filters.text & filters.incoming)
 async def private_give_filter(bot, message):
         await auto_filter(bot, message)
 
