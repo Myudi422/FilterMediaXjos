8264748deba8a971f7d1473e8a69b9ced8b08f6f
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-06-08 19:03:22 +0700

    yrd

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Myudi422/FilterMediaXjos.git /IMDb-Movie-Bot
+git clone -b master https://github.com/Myudi422/FilterMediaXjos.git /ccgnimex
 cd /ccgnimex
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
