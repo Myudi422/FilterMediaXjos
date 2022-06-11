11e7b51e213bc52fc581bb88bcfba00942ac732e
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-06-11 11:24:26 +0700

    tes

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -24,26 +24,26 @@ async def allowed(_, __, message):
 async def gen_link_s(bot, message):
     replied = message.reply_to_message
     if not replied:
-        return await message.reply('Reply to a message to get a shareable link.')
+        return await message.reply('Balas pesan untuk mendapatkan tautan yang dapat dibagikan.')
     file_type = replied.media
     if file_type not in ["video", 'audio', 'document']:
-        return await message.reply("Reply to a supported media")
+        return await message.reply("Balas ke media yang didukung")
     if message.has_protected_content and message.chat.id not in ADMINS:
         return await message.reply("okDa")
     file_id, ref = unpack_new_file_id((getattr(replied, file_type)).file_id)
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    await message.reply(f"Here is your Link:\nhttps://t.me/{temp.U_NAME}?start={outstr}")
+    await message.reply(f"Ini Tautan Anda:\nhttps://t.me/{temp.U_NAME}?start={outstr}")
     
     
 @Client.on_message(filters.command(['filebatch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/filebatch https://t.me/imdbprobots/6 https://t.me/imdbprobots/50</code>.")
+        return await message.reply("Gunakan format yang benar.\nContoh <code>/filebatch atau /pbatch https://t.me/ccgnimexdb/22 https://t.me/ccgnimexdb/26</code>.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample <code>/filebatch https://t.me/imdbprobots/6 https://t.me/imdbprobots/50</code>.")
+        return await message.reply("Gunakan format yang benar.\nContoh <code>/filebatch atau /pbatch https://t.me/ccgnimexdb/22 https://t.me/ccgnimexdb/26/code>.")
     cmd, first, last = links
     regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
     match = regex.match(first)
@@ -67,17 +67,17 @@ async def gen_link_batch(bot, message):
     try:
         chat_id = (await bot.get_chat(f_chat_id)).id
     except ChannelInvalid:
-        return await message.reply('This may be a private channel / group. Make me an admin over there to index the files.')
+        return await message.reply('Ini mungkin saluran / grup pribadi. Jadikan saya admin di sana untuk mengindeks file.')
     except (UsernameInvalid, UsernameNotModified):
-        return await message.reply('Invalid Link specified.')
+        return await message.reply('Tautan tidak valid ditentukan.')
     except Exception as e:
         return await message.reply(f'Errors - {e}')
 
-    sts = await message.reply("Generating link for your message.\nThis may take time depending upon number of messages")
+    sts = await message.reply("Membuat tautan untuk pesan Anda.\Ini mungkin memakan waktu tergantung pada jumlah pesan")
     if chat_id in FILE_STORE_CHANNEL:
         string = f"{f_msg_id}_{l_msg_id}_{chat_id}_{cmd.lower().strip()}"
         b_64 = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-        return await sts.edit(f"Here is your link https://t.me/{temp.U_NAME}?start=DSTORE-{b_64}")
+        return await sts.edit(f"Ini linkmu https://t.me/{temp.U_NAME}?start=KunciFile-{b_64}")
 
     FRMT = "Generating Link...\nTotal Messages: `{total}`\nDone: `{current}`\nRemaining: `{rem}`\nStatus: `{sts}`"
 
@@ -122,4 +122,4 @@ async def gen_link_batch(bot, message):
     post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
-    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id}")
+    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=Berkas-{file_id}")
