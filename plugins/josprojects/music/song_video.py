6e4c7bda10f928c3bbcd9835c2c5ccc6f41677b5
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-08-23 11:28:05 +0700

    tes

--- plugins/josprojects/music/song_video.py
+++ plugins/josprojects/music/song_video.py
@@ -251,62 +251,67 @@ def time_to_seconds(time):
 @Client.on_message(filters.command(["vsong", "video", "mp4"]))
 async def vsong(client, message: Message):
     urlissed = get_text(message)
-    reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
-
-    pablo = await client.send_message(
-        message.chat.id, f"**🔎 Mencari..** `{urlissed}`", reply_to_message_id=reply_id
-    )
     if not urlissed:
-        await pablo.edit("Sintaks Perintah Tidak Valid Silakan Periksa Menu bantuan Untuk Mengetahui Lebih Lanjut!")
+        await client.send_message(
+            message.chat.id,
+            "Invalid Command Syntax, Please Check Help Menu To Know More!",
+        )
         return
-
+    pablo = await client.send_message(
+        message.chat.id, f"`Getting {urlissed} From Youtube Servers. Please Wait.`"
+    )
     search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
     mi = search.result()
     mio = mi["search_result"]
     mo = mio[0]["link"]
+    mio[0]["duration"]
     thum = mio[0]["title"]
     fridayz = mio[0]["id"]
-    mio[0]["channel"]
+    thums = mio[0]["channel"]
     kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
     await asyncio.sleep(0.6)
-    url = mo
     sedlyf = wget.download(kekme)
     opts = {
-        "format": "best",
+        "format": "bestaudio",
         "addmetadata": True,
         "key": "FFmpegMetadata",
+        "writethumbnail": True,
         "prefer_ffmpeg": True,
         "geo_bypass": True,
         "nocheckcertificate": True,
-        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
-        "outtmpl": "%(id)s.mp4",
-        "logtostderr": False,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "720",
+            }
+        ],
+        "outtmpl": "%(id)s.mp3",
         "quiet": True,
+        "logtostderr": False,
     }
     try:
         with YoutubeDL(opts) as ytdl:
-            ytdl_data = ytdl.extract_info(url, download=True)
+            ytdl_data = ytdl.extract_info(mo, download=True)
     except Exception as e:
-        await event.edit(event, f"**Download Gagal** \n**Error :** `{str(e)}`")
+        await pablo.edit(f"**Failed To Download** \n**Error :** `{str(e)}`")
         return
     c_time = time.time()
-    file_stark = f"{ytdl_data['id']}.mp4"
-    capy = f"""
-**🏷️ Video :** [{thum}]({mo})
-"""
-    await client.send_video(
-        message.chat.id, reply_to_message_id=reply_id,
-        video=open(file_stark, "rb"),
+    capy = f"**Song Name :** [{thum}]({mo}) \n**Requested For :** `{urlissed}` \n**Uploaded By :** @Dr_Assad_Ali "
+    file_stark = f"{ytdl_data['id']}.mp3"
+    await client.send_audio(
+        message.chat.id,
+        audio=open(file_stark, "rb"),
         duration=int(ytdl_data["duration"]),
-        file_name=str(ytdl_data["title"]),
+        title=str(ytdl_data["title"]),
+        performer=str(ytdl_data["uploader"]),
         thumb=sedlyf,
         caption=capy,
-        supports_streaming=True,
         progress=progress,
         progress_args=(
             pablo,
             c_time,
-            f"**📥 Download** `{urlissed}`",
+            f"`Uploading {urlissed} Song From YouTube Music!`",
             file_stark,
         ),
     )
