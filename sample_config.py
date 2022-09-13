ac3facaea334db4f477a237676f473dd830ca4c9
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-09-13 20:23:11 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,4 +2,4 @@ import os
 
 
 class Config(object):
-    RemoveBG_API = os.environ.get("RemoveBG_API", "UDB4cWaSQcFfvXsSbPnwuUur")
+    RemoveBG_API = os.environ.get("RemoveBG_API", "DncxpiPpGhxqdQB9eF4H83qo")
